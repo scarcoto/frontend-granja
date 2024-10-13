1d700f9bb466512e8b96e8e2a35468b7d0ef87b8
scarlet <scar.andrecoto@gmail.com>
2024-10-12 23:25:01 -0600

    feat: implementacion de frontend modulo produccion huevos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProduccionHuevosFormComponent } from './produccion-huevos-form/produccion-huevos-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProduccionHuevosFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProduccionHuevosFormComponent } from './produccion-huevos-form/produccion-huevos-form.component';
+import { ProduccionHuevosListComponent } from './produccion-huevo-listado/produccion-huevo-listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',
+        component: ProduccionHuevosListComponent
+    },
+    { 
+        path: 'crear',
+        component: ProduccionHuevosFormComponent
+    },
+
+];
